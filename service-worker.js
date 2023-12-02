80e3127974d97adcdf9a800bf68ab5db47b2cb10
Alex <142623358+Alexidians@users.noreply.github.com>
2023-12-02 14:12:14 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'offline-api';
+const CACHE_DATA = [
+  '/service-worker.js',
+  '/create.html',
+  '/delete.html',
+  '/get.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_DATA);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
